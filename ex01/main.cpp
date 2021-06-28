c4003710c12d265220d6e78ccb9fb6d83be73bde
Morgan Roux <roux.morgan@gmail.com>
2021-06-29 00:49:43 +0200

    ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,25 +6,77 @@
 /*   By: mroux <mroux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 23:42:29 by mroux             #+#    #+#             */
-/*   Updated: 2021/06/29 00:03:01 by mroux            ###   ########.fr       */
+/*   Updated: 2021/06/29 00:49:09 by mroux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.hpp"
 
-uintptr_t	serialize(Data* ptr)
+uintptr_t	serialize(Data* data)
 {
-	return reinterpret_cast<uintptr_t> (ptr);
+	uintptr_t 	ptr = reinterpret_cast<uintptr_t> (new char[8 + 4 + 8]);
+	for (int i = 0, j = 0; i < 8; i++, j++)
+	{
+		char *str = reinterpret_cast<char *> (ptr);
+		str[i] = data->str1[j];
+	}
+	for (int i = 8, j = 0; j < 4; i++, j++)
+	{
+		char *str = reinterpret_cast<char *> (ptr);
+		str[i] = data->str2[j];
+	}
+	for (int i = 12, j = 0 ; j < 2; i++, j++)
+	{
+		char	*p_i = reinterpret_cast<char *>(&data->sh);
+		char 	*str = reinterpret_cast<char *> (ptr);
+		str[i] = p_i[j];
+	}
+	for (int i = 14, j = 0 ; j < 4; i++, j++)
+	{
+		char	*p_i = reinterpret_cast<char *>(&data->i);
+		char 	*str = reinterpret_cast<char *> (ptr);
+		str[i] = p_i[j];
+	}
+	for (int i = 18, j = 0; j < 8; i++, j++)
+	{
+		char *str = reinterpret_cast<char *> (ptr);
+		str[i] = data->str3[j];
+	}
+
+	return ptr;
 }
 
-Data*		deserialize(uintptr_t raw)
+Data*		deserialize(uintptr_t ptr)
 {
-	return reinterpret_cast<Data *> (raw);
+	Data *data = new Data;
+
+	for (int i = 0, j = 0; i < 8; i++, j++)
+	{
+		data->str1[j] = reinterpret_cast<char *> (ptr)[i];
+	}
+	for (int i = 8, j = 0; j < 4; i++, j++)
+	{
+		data->str2[j] = reinterpret_cast<char *> (ptr)[i];
+	}
+	for (int i = 12, j = 0 ; j < 2; i++, j++)
+	{
+		char	*p_i = reinterpret_cast<char *>(&data->sh);
+		p_i[j] = reinterpret_cast<char *> (ptr)[i];
+	}
+	for (int i = 14, j = 0 ; j < 4; i++, j++)
+	{
+		char	*p_i = reinterpret_cast<char *>(&data->i);
+		p_i[j] = reinterpret_cast<char *> (ptr)[i];
+	}
+	for (int i = 18, j = 0; j < 8; i++, j++)
+	{
+		data->str3[j] = reinterpret_cast<char *> (ptr)[i];
+	}
+	return data;
 }
 
 void		dump(char *str)
 {
-	std::cout << "Dump:" << std::endl;
 	for (size_t i = 0; i < 30; i++)
 	{
 		if (static_cast<int> (str[i]) < 32 || static_cast<int> (str[i])  > 126)
@@ -40,12 +92,20 @@ int main()
 	Data		data, *pdata;
 	uintptr_t	ptr;
 
-	data.name = "Essai";
-	data.i = 100;
+	memcpy(data.str1, "abcdefg", 8);
+	memcpy(data.str2, "hij", 4);
+	memcpy(data.str3, "klmnopq", 8);
+	data.sh = 0x4546;    //FE
+	data.i = 0x44434241; //ABCD
 
+	std::cout << data.str1 << " - " << data.str2 << " - " << data.str3 << " - " << data.i << " - " << data.sh << std::endl;
+	std::cout << "Dump struct :" << std::endl;
+	dump(reinterpret_cast<char *>(&data));
 	ptr = serialize(&data);
+	std::cout << "Dump serial :" << std::endl;
 	dump(reinterpret_cast<char *>(ptr));
 	pdata = deserialize(ptr);
-
-	std::cout << pdata->name << " - " << pdata->i << std::endl;
+	std::cout << "Dump deserialized :" << std::endl;
+	dump(reinterpret_cast<char *>(pdata));
+	std::cout << pdata->str1 << " - " << pdata->str2 << " - " << pdata->str3 << " - " << pdata->i << " - " << pdata->sh << std::endl;
 }
--- ex01/main.hpp
+++ ex01/main.hpp
@@ -6,15 +6,20 @@
 /*   By: mroux <mroux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 23:42:48 by mroux             #+#    #+#             */
-/*   Updated: 2021/06/28 23:52:28 by mroux            ###   ########.fr       */
+/*   Updated: 2021/06/29 00:45:48 by mroux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cstring>
+#include <cstdio>
 
 typedef struct		s_data {
-		std::string	name;
-		int			i;
+		char 	str1[8];
+		char 	str2[4];
+		short	sh;
+		int		i;
+		char 	str3[8];
 }					Data;
 
 uintptr_t			serialize(Data* ptr);
